38a395b6bd1e149907050b15107b7e0e62e1ab6a
Lincoln Wentz <linkwentz@gmail.com>
2023-06-30 21:03:47 -0500

    Change quest content size and spacing

--- client/src/components/Quest.jsx
+++ client/src/components/Quest.jsx
@@ -4,7 +4,9 @@ function Quest() {
   return(
     <div className="Quest Glass">
         <header>Title Text</header>
+        <div className="break"/>
         <main>Text describing whatever quest you're on, why you're on it, why you have to do what you have to do, what you feel about it, etc.</main>
+        <div className="break"/>
         <footer>
             <form>
                 <Objective>Objective Text</Objective>
