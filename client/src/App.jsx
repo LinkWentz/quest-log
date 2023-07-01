2fe272caee1efbb90aa57f41fb4927b6b4604412
Lincoln Wentz <linkwentz@gmail.com>
2023-07-01 11:12:43 -0500

    Make quest log position respond to mouse movement

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,29 @@
 import './App.css'
 import BackgroundImage from './components/BackgroundImage'
 import QuestLog from './components/QuestLog'
+import { useState, useEffect, createContext } from 'react'
+
+export const MousePositionContext = createContext({x: 0.5, y: 0.5});
 
 function App() {
+
+  const [mousePositionState, setMousePositionState] = useState({x: 0.5, y: 0.5});
+
+  useEffect(() => {
+    const setMousePosition = ({x, y}) => {
+      setMousePositionState({x: x/window.innerWidth, y: y/window.innerHeight});
+    }
+    window.addEventListener('mousemove', setMousePosition);
+    return (() => {
+      window.addEventListener('mousemove', setMousePosition);
+    })
+  }, [])
+
   return (
-    <>
+    <MousePositionContext.Provider value={mousePositionState}>
       <BackgroundImage/>
       <QuestLog/>
-    </>
+    </MousePositionContext.Provider>
   )
 }
 
--- client/src/components/QuestLog.jsx
+++ client/src/components/QuestLog.jsx
@@ -1,10 +1,15 @@
 import './styles/QuestLog.css'
 import QuestSelector from './QuestSelector'
 import Quest from './Quest'
+import { useContext } from 'react'
+import { MousePositionContext } from '../App'
 
 function QuestLog() {
+
+  const mousePosition = useContext(MousePositionContext);
+
   return (
-    <div className="QuestLog">
+    <div className="QuestLog" style={{top: `${-(mousePosition.y-0.5) * 1.5}%`, left: `${-(mousePosition.x-0.5) * 1.5}%`}}>
       <QuestSelector/>
       <Quest/>
     </div>
