12d333ffd3e3c5027d257b96fb413bc330786b45
Lincoln Wentz <linkwentz@gmail.com>
2023-08-04 00:05:46 -0500

    Add database access to api router

--- server/api.js
+++ server/api.js
@@ -1,10 +1,24 @@
 require('dotenv').config();
 
+// Packages
 const express = require('express');
+const pg = require('pg');
 
+/*---- Config -----*/
+// Router Setup
 const api = express.Router();
 
-api.get('/:user', (req, res) => {
+// Database Access Setup
+const pool = new pg.Pool({
+    user: process.env['PGUSER'],
+    host: process.env['PGHOST'],
+    database: process.env['PGDATABASE'],
+    password: process.env['PGPASSWORD'],
+    port: process.env['PGPORT']
+});
+
+/*---- Routes -----*/
+api.get('/:user', async (req, res) => {
     res.send('User ID: ' + req.params.user);
 });
 
