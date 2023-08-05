9f55def60f7ad75192bf7812aedea355e7e0a995
Lincoln Wentz <linkwentz@gmail.com>
2023-08-05 01:00:44 -0500

    Move repeated error handling to wrapper function

--- server/api.js
+++ server/api.js
@@ -26,13 +26,11 @@ api.use('/:user', (req, res, next) => {
         return;
     }
     next();
-})
+});
 
-// Quest Logs
-api.get('/:user', async (req, res) => {
+const queryResponse = async (queryFunction, req, res) => {
     try {
-        let result = await pool.query(`SELECT * FROM quest_logs WHERE user_id = ${req.params.user} ORDER BY created_at`);
-        result = result.rows;
+        const result = await queryFunction();
 
         if (!result) {
             res.status(404);
@@ -47,69 +45,45 @@ api.get('/:user', async (req, res) => {
         res.send([]);
     }
     return;
+};
+
+// Quest Logs
+api.get('/:user', async (req, res) => {
+    queryResponse(async () => {
+        let result = await pool.query(`SELECT * FROM quest_logs WHERE user_id = ${req.params.user} ORDER BY created_at`);
+        result = result.rows;
+
+        return result;
+    }, req, res);
 });
 
 api.get('/:user/:questLog', async (req, res) => {
-    try {
+    queryResponse(async () => {
         let result = await pool.query(`SELECT * FROM quests WHERE log_id = ${req.params.questLog} ORDER BY created_at`);
         result = result.rows;
 
         result = await result.slice(parseInt(req.query.start) || 0, ((parseInt(req.query.start) || 0) + parseInt(req.query.limit)) || -1);
-        console.log(((req.query.start || 0) + req.query.limit) || -1);
 
-        if (!result) {
-            res.status(404);
-            res.send([]);
-            return;
-        }
-
-        res.send(result);
-    }
-    catch {
-        res.status(404);
-        res.send([]);
-    }
-    return;
+        return result;
+    }, req, res);
 });
 
 api.get('/:user/:questLog/:quest', async (req, res) => {
-    try {
+    queryResponse(async () => {
         let result = await pool.query(`SELECT * FROM steps WHERE quest_id = ${req.params.quest} AND position_in_quest = ${req.query.step || '(SELECT MAX(position_in_quest) FROM steps)'} ORDER BY position_in_quest LIMIT 1`);
         result = result.rows;
 
-        if (!result) {
-            res.status(404);
-            res.send([]);
-            return;
-        }
-
-        res.send(result);
-    }
-    catch {
-        res.status(404);
-        res.send([]);
-    }
-    return;
+        return result;
+    }, req, res);
 });
 
 api.get('/:user/:questLog/:quest/:step', async (req, res) => {
-    try {
+    queryResponse(async () => {
         let result = await pool.query(`SELECT * FROM objectives WHERE step_id = ${req.params.step} ORDER BY created_at`);
         result = result.rows;
 
-        if (!result) {
-            res.status(404);
-            res.send([]);
-            return;
-        }
-
-        res.send(result);
-    }
-    catch {
-        res.status(404);
-        res.send([]);
-    }
-    return;
+        return result;
+    }, req, res);
 })
 
 module.exports = api;
