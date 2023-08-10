b7df749a50d9cff99f539128bd02c9c040435709
Lincoln Wentz <linkwentz@gmail.com>
2023-08-09 23:57:10 -0500

    Refactor routes to remove repeated response code

--- server/api.js
+++ server/api.js
@@ -20,317 +20,166 @@ const pool = new pg.Pool({
 /*---- Routes -----*/
 // Middleware
 api.use('/:user', (req, res, next) => {
-    if (req.params.user == 0 || req.user.id == req.params.user){
+    if (req.params.user == 0 || req.user.id == req.params.user) {
         return next();
     }
-    res.status(401).send();
-    return;
+    return res.status(401).send();
 });
 
 // Get Routes
-api.get('/:user', async (req, res) => {
-    try {
-        const query = `SELECT * FROM quest_logs
-                       WHERE user_id = ${req.params.user} 
-                       ORDER BY created_at`;
-
-        let result = await pool.query(query);
-        result = result.rows;
-
-        if (result.length == 0) {
-            res.status(404).send();   
-        }
-
-        res.status(200).send(result);
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).send();
-    }
-    return;
+api.get('/:user', (req, res, next) => {
+    pool.query(`SELECT * FROM quest_logs
+                WHERE user_id = ${req.params.user} 
+                ORDER BY created_at`)
+    .then((result) => {
+        res.locals.result = result;
+        next();
+    })
+    .catch(next);
 });
 
-api.get('/:user/:questLog', async (req, res) => {
-    try {
-        const query = `SELECT * FROM quests 
-                       WHERE log_id = ${req.params.questLog} 
-                       ORDER BY created_at
-                       OFFSET ${parseInt(req.query.start) || 0}
-                       LIMIT ${parseInt(req.query.limit) || 10}`;
-
-        let result = await pool.query(query);
-        result = result.rows;
-
-        if (result.length == 0) {
-            res.status(404).send();   
-        }
-
-        res.status(200).send(result);
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).send();
-    }
-    return;
+api.get('/:user/:questLog', (req, res, next) => {
+    pool.query(`SELECT * FROM quests 
+                WHERE log_id = ${req.params.questLog} 
+                ORDER BY created_at
+                OFFSET ${parseInt(req.query.start) || 0}
+                LIMIT ${parseInt(req.query.limit) || 10}`)
+    .then((result) => {
+        res.locals.result = result;
+        next();
+    })
+    .catch(next);
 });
 
-api.get('/:user/:questLog/:quest', async (req, res) => {
-    try {
-        const query = `SELECT * FROM steps 
-                       WHERE quest_id = ${req.params.quest}
-                       ORDER BY created_at DESC
-                       OFFSET ${parseInt(req.query.step) || 0}
-                       LIMIT 1`;
-
-        let result = await pool.query(query);
-        result = result.rows;
-
-        if (result.length == 0) {
-            res.status(404).send();   
-        }
-
-        res.status(200).send(result);
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).send();
-    }
-    return;
+api.get('/:user/:questLog/:quest', (req, res, next) => {
+    pool.query(`SELECT * FROM steps 
+                WHERE quest_id = ${req.params.quest}
+                ORDER BY created_at DESC
+                OFFSET ${parseInt(req.query.step) || 0}
+                LIMIT 1`)
+    .then((result) => {
+        res.locals.result = result;
+        next();
+    })
+    .catch(next);
 });
 
-api.get('/:user/:questLog/:quest/:step', async (req, res) => {
-    try {
-        const query = `SELECT * FROM objectives 
-                       WHERE step_id = ${req.params.step} 
-                       ORDER BY created_at`
-                       
-        let result = await pool.query(query);
-        result = result.rows;
-
-        if (result.length == 0) {
-            res.status(404).send();   
-        }
-
-        res.status(200).send(result);
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).send();
-    }
-    return;
+api.get('/:user/:questLog/:quest/:step', (req, res, next) => {
+    pool.query(`SELECT * FROM objectives 
+                WHERE step_id = ${req.params.step} 
+                ORDER BY created_at`)
+    .then((result) => {
+        res.locals.result = result;
+        next();
+    })
+    .catch(next);
 });
+
+api.get('/:user', (req, res) => {
+    if (res.locals.result.rows.length == 0) {
+        return res.status(404).send();
+    }
+
+    return res.status(200).send(res.locals.result.rows);
+})
 
 // Post Routes
-api.post('/:user', async (req, res) => {
-    try {
-        const query = `INSERT INTO quest_logs
-                       (id, 
-                        user_id, 
-                        title, 
-                        created_at)
-                       VALUES 
-                       (DEFAULT, 
-                        ${req.params.user}, 
-                        '${req.body.quest_log_title}', 
-                        NOW())`;
-        let result = await pool.query(query);
-
-        res.status(201).send();
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).send();
-    }
+api.post('/:user', (req, res, next) => {
+    pool.query(`INSERT INTO quest_logs
+                (id, user_id, title, created_at)
+                VALUES 
+                (DEFAULT, ${req.params.user}, '${req.body.quest_log_title}', NOW())`)
+    .then(() => {next()}).catch(next);
 });
 
-api.post('/:user/:questLog', async (req, res) => {
-    try {
-        const query = `INSERT INTO quests
-                       (id, 
-                        log_id, 
-                        title, 
-                        created_at)
-                       VALUES 
-                       (DEFAULT, 
-                        ${req.params.questLog}, 
-                        '${req.body.quest_title}', 
-                        NOW())`;
-        let result = await pool.query(query);
-
-        res.status(201).send();
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).send();
-    }
+api.post('/:user/:questLog', (req, res, next) => {
+    pool.query(`INSERT INTO quests
+                (id, log_id, title, created_at)
+                VALUES 
+                (DEFAULT, ${req.params.questLog}, '${req.body.quest_title}', NOW())`)
+    .then(() => {next()}).catch(next);
 });
 
-api.post('/:user/:questLog/:quest', async (req, res) => {
-    try {
-        const query = `INSERT INTO steps
-                       (id, 
-                        quest_id,
-                        title, 
-                        body, 
-                        created_at)
-                       VALUES 
-                       (DEFAULT, 
-                        ${req.params.quest},
-                        '${req.body.step_title}',
-                        '${req.body.step_body}',
-                        NOW())`;
-        let result = await pool.query(query);
-
-        res.status(201).send();
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).send();
-    }
+api.post('/:user/:questLog/:quest', (req, res, next) => {
+    pool.query(`INSERT INTO steps
+                (id, quest_id, title, body, created_at)
+                VALUES 
+                (DEFAULT, ${req.params.quest}, '${req.body.step_title}', '${req.body.step_body}', NOW())`)
+    .then(() => {next()}).catch(next);
 });
 
-api.post('/:user/:questLog/:quest/:step', async (req, res) => {
-    try {
-        const query = `INSERT INTO objectives
-                       (id, 
-                        step_id,
-                        statement,
-                        complete,
-                        created_at)
-                       VALUES 
-                       (DEFAULT, 
-                        ${req.params.step}, 
-                        '${req.body.objective_statement}',
-                        ${req.body.objective_complete},
-                        NOW())`;
-        let result = await pool.query(query);
+api.post('/:user/:questLog/:quest/:step', (req, res, next) => {
+    pool.query(`INSERT INTO objectives
+                (id, step_id, statement, complete, created_at)
+                VALUES
+                (DEFAULT, ${req.params.step}, '${req.body.objective_statement}', ${req.body.objective_complete}, NOW())`)
+    .then(() => {next()}).catch(next);
+});
 
-        res.status(201).send();
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).send();
-    }
+api.post('/:user', (req, res) => {
+    return res.status(201).send();
 });
 
 // Patch Routes
-api.patch('/:user/:questLog', async (req, res) => {
-    try {
-        const query = `UPDATE quest_logs
-                       SET title = '${req.body.quest_log_title}'
-                       WHERE id = ${req.params.questLog}`;
-        let result = await pool.query(query);
-
-        res.status(201).send();
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).send();
-    }
+api.patch('/:user/:questLog', (req, res, next) => {
+    pool.query(`UPDATE quest_logs
+                SET title = '${req.body.quest_log_title}'
+                WHERE id = ${req.params.questLog}`)
+    .then(() => {next()}).catch(next);
 });
 
-api.patch('/:user/:questLog/:quest', async (req, res) => {
-    try {
-        const query = `UPDATE quests
-                       SET title = '${req.body.quest_title}'
-                       WHERE id = ${req.params.quest}`;
-        let result = await pool.query(query);
-
-        res.status(201).send();
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).send();
-    }
+api.patch('/:user/:questLog/:quest', (req, res, next) => {
+    pool.query(`UPDATE quests
+                SET title = '${req.body.quest_title}'
+                WHERE id = ${req.params.quest}`)
+    .then(() => {next()}).catch(next);
 });
 
-api.patch('/:user/:questLog/:quest/:step', async (req, res) => {
-    try {
-        const query = `UPDATE steps
-                       SET title = '${req.body.step_title}', 
-                           body = '${req.body.step_body}'
-                       WHERE id = ${req.params.step}`;
-        let result = await pool.query(query);
-
-        res.status(201).send();
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).send();
-    }
+api.patch('/:user/:questLog/:quest/:step', (req, res, next) => {
+    pool.query(`UPDATE steps
+                SET title = '${req.body.step_title}', body = '${req.body.step_body}'
+                WHERE id = ${req.params.step}`)
+    .then(() => {next()}).catch(next);
 });
 
-api.patch('/:user/:questLog/:quest/:step/:objective', async (req, res) => {
-    try {
-        const query = `UPDATE objectives
-                       SET statement = '${req.body.objective_statement}', 
-                           complete = ${req.body.objective_complete}
-                       WHERE id = ${req.params.objective}`;
-        let result = await pool.query(query);
+api.patch('/:user/:questLog/:quest/:step/:objective', (req, res, next) => {
+    pool.query(`UPDATE objectives
+                SET statement = '${req.body.objective_statement}', complete = ${req.body.objective_complete}
+                WHERE id = ${req.params.objective}`)
+    .then(() => {next()}).catch(next);
+});
 
-        res.status(201).send();
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).send();
-    }
+api.patch('/:user', (req, res) => {
+    return res.status(201).send();
 });
 
 // Delete Routes
-api.delete('/:user/:questLog', async (req, res) => {
-    try {
-        const query = `DELETE FROM quest_logs
-                       WHERE id = ${req.params.questLog}`;
-        let result = await pool.query(query);
-
-        res.status(200).send();
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).send();
-    }
+api.delete('/:user/:questLog', (req, res, next) => {
+    pool.query(`DELETE FROM quest_logs
+                WHERE id = ${req.params.questLog}`)
+    .then(() => {next()}).catch(next);
 });
 
-api.delete('/:user/:questLog/:quest', async (req, res) => {
-    try {
-        const query = `DELETE FROM quests
-                       WHERE id = ${req.params.quest}`;
-        let result = await pool.query(query);
-
-        res.status(200).send();
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).send();
-    }
+api.delete('/:user/:questLog/:quest', (req, res, next) => {
+    pool.query(`DELETE FROM quests
+                WHERE id = ${req.params.quest}`)
+    .then(() => {next()}).catch(next);
 });
 
-api.delete('/:user/:questLog/:quest/:step', async (req, res) => {
-    try {
-        const query = `DELETE FROM steps
-                       WHERE id = ${req.params.step}`;
-        let result = await pool.query(query);
-
-        res.status(200).send();
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).send();
-    }
+api.delete('/:user/:questLog/:quest/:step', (req, res, next) => {
+    pool.query(`DELETE FROM steps
+                WHERE id = ${req.params.step}`)
+    .then(() => {next()}).catch(next);
 });
 
-api.delete('/:user/:questLog/:quest/:step/:objective', async (req, res) => {
-    try {
-        const query = `DELETE FROM objectives
-                       WHERE id = ${req.params.objective}`;
-        let result = await pool.query(query);
+api.delete('/:user/:questLog/:quest/:step/:objective', (req, res, next) => {
+    pool.query(`DELETE FROM objectives
+                WHERE id = ${req.params.objective}`)
+    .then(() => {next()}).catch(next);
+});
 
-        res.status(200).send();
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).send();
-    }
+api.delete('/:user', (req, res) => {
+    return res.status(200).send();
 });
 
 module.exports = api;
