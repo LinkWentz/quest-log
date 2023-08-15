c0afc3a61811451458e7609b174b60b1bba13a31
Lincoln Wentz <linkwentz@gmail.com>
2023-08-14 21:44:15 -0500

    Make background react to mouse movement instead of quest log

--- client/src/components/BackgroundImage.jsx
+++ client/src/components/BackgroundImage.jsx
@@ -1,13 +1,22 @@
 import './styles/BackgroundImage.css'
 import { useState } from 'react'
+import { useContext } from 'react'
+import { MousePositionContext } from '../App'
 
 function BackgroundImage() {
 
     const [color, setColor] = useState('#ffff00');
     const [backgroundImage, setBackgroundImage] = useState(null);
 
+    const mousePosition = useContext(MousePositionContext);
+
     return (
-      <div className="BackgroundImage" style={{backgroundColor: color, backgroundImage: `url(${backgroundImage})`}}/>
+      <div className="BackgroundImage" style={{
+        backgroundColor: color,
+        backgroundImage: `url(${backgroundImage})`,
+        top: `${-(mousePosition.y-0.5) * 1.5 - 5}%`,
+        left: `${-(mousePosition.x-0.5) * 1.5 - 5}%`
+      }}/>
     );
   }
   
--- client/src/components/QuestLog.jsx
+++ client/src/components/QuestLog.jsx
@@ -1,15 +1,11 @@
 import './styles/QuestLog.css'
 import QuestSelector from './QuestSelector'
 import Quest from './Quest'
-import { useContext } from 'react'
-import { MousePositionContext } from '../App'
 
 function QuestLog() {
 
-  const mousePosition = useContext(MousePositionContext);
-
   return (
-    <div className="QuestLog" style={{top: `${-(mousePosition.y-0.5) * 1.5}%`, left: `${-(mousePosition.x-0.5) * 1.5}%`}}>
+    <div className="QuestLog">
       <QuestSelector/>
       <Quest/>
     </div>
