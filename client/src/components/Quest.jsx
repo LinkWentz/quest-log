5c09d93cbfe375887abcfc03f12a1846bfe0c2fc
Lincoln Wentz <linkwentz@gmail.com>
2023-08-15 22:59:57 -0500

    Add forward and back buttons to quest

--- client/src/components/Quest.jsx
+++ client/src/components/Quest.jsx
@@ -3,15 +3,21 @@ import Objective from './Objective';
 function Quest() {
     return (
         <div className="Quest Glass">
-            <header>Title Text</header>
-            <div className="break" />
-            <main>Text describing whatever quest you're on, why you're on it, why you have to do what you have to do, what you feel about it, etc.</main>
-            <div className="break" />
-            <footer>
-                <Objective/>
-                <Objective/>
-                <Objective/>
-            </footer>
+            <section>
+                <header>Title Text</header>
+                <div className="break" />
+                <main>Text describing whatever quest you're on, why you're on it, why you have to do what you have to do, what you feel about it, etc.</main>
+                <div className="break" />
+                <footer>
+                    <Objective/>
+                    <Objective/>
+                    <Objective/>
+                </footer>
+            </section>
+            <nav>
+                <button className="next Interactable"/>
+                <button className="last Interactable"/>
+            </nav>
         </div>
     );
 }
