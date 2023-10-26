7adf5c55d2608c79d995f6040df4cd744daaed5a
Lincoln Wentz <linkwentz@gmail.com>
2023-10-26 14:59:25 -0500

    Add api interaction for background image selection

--- client/src/components/SettingsMenu.jsx
+++ client/src/components/SettingsMenu.jsx
@@ -1,14 +1,38 @@
 import './styles/SettingsMenu.css'
-import { BackgroundImageURLContext } from '../App'
-import { useContext } from 'react';
+import { BackgroundImageURLContext, SelectedIDsContext } from '../App'
+import { useContext, useEffect } from 'react';
 
 function SettingsMenu() {
     const { setBackgroundImageURL } = useContext(BackgroundImageURLContext);
+    const { selectedIDs } = useContext(SelectedIDsContext);
     
     const onInput = (event) => {
         const newURL = event.target.value;
+
         setBackgroundImageURL(newURL);
+        
+        (async () => {
+            await fetch(`http://localhost:3000/questlogs/${selectedIDs.selectedQuestLogID}/backgroundimageurl`, {
+                method: 'PATCH',
+                body: JSON.stringify({
+                    quest_log_background_image_url: newURL
+                }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                }
+            })
+        })();
     }
+
+    useEffect(() => {
+        if (selectedIDs.selectedQuestLogID) {
+            (async () => {
+                let selectedQuestLog = {};
+                selectedQuestLog = await (await fetch(`http://localhost:3000/questlog/${selectedIDs.selectedQuestLogID}`)).json();
+                await setBackgroundImageURL(selectedQuestLog[0].background_image_url);
+            })();
+        }
+    }, [selectedIDs]);
 
     return (
         <div className="SettingsMenu Glass" tabIndex='0'>
