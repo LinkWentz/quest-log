c6359325b22de2406fbf0eda11ab649ef3c2f17e
Lincoln Wentz <linkwentz@gmail.com>
2023-11-07 12:16:54 -0600

    Add rough api interaction and navigation

--- client/src/components/StepDisplay.jsx
+++ client/src/components/StepDisplay.jsx
@@ -1,12 +1,70 @@
 import Objective from './Objective';
+import EditableText from './EditableText';
+import { useEffect, useState, useContext } from 'react';
+import { SelectedIDsContext } from '../App';
+import API from '../scripts/API';
 
 function StepDisplay() {
+
+    const { selectedIDs } = useContext(SelectedIDsContext);
+
+    const [steps, setSteps] = useState([{
+        title: '',
+        body: ''
+    }]);
+    const [currentStep, setCurrentStep] = useState(0);
+    const [content, setContent] = useState({
+        title: '',
+        body: ''
+    });
+
+    const refreshStepList = async () => {
+        const newSteps = selectedIDs.selectedQuestID ? await API.get.stepsForQuest(selectedIDs.selectedQuestID) : [];
+        setSteps(newSteps);
+    }
+
+    const createNewStep = async () => {
+        await API.create.stepForQuest(selectedIDs.selectedQuestID);
+        refreshStepList();
+    }
+
+    const updateStepData = ({ title, body }) => {
+        API.update.step(content.stepID, { title, body });
+    }
+
+    const deleteStep = async () => {
+        await API.delete.step(content.stepID);
+        previousStep();
+    }
+
+    const nextStep = () => {
+        setCurrentStep(currentStep > 0 ? currentStep - 1 : 0);
+    }
+
+    const previousStep = () => {
+        setCurrentStep(currentStep < steps.length - 1 ? currentStep + 1 : steps.length - 1);
+    }
+
+    useEffect(() => {
+        if (steps.length > 0) {
+            setContent(steps[currentStep]);
+        }
+    }, [currentStep, steps]);
+
+    useEffect(() => {
+        updateStepData(content);
+    }, [content]);
+
+    useEffect(() => {
+        refreshStepList();
+    }, [selectedIDs]);
+
     return (
         <div className="StepDisplay Glass">
             <section>
-                <header>Title Text</header>
+                <header><EditableText onContentChange={(newText) => {setContent({...content, title: newText})}}>{content.title || '...'}</EditableText></header>
                 <div className="break" />
-                <main>Text describing whatever quest you're on, why you're on it, why you have to do what you have to do, what you feel about it, etc.</main>
+                <main><EditableText onContentChange={(newText) => {setContent({...content, body: newText})}}>{content.body || '...'}</EditableText></main>
                 <div className="break" />
                 <footer>
                     <Objective/>
@@ -16,8 +74,8 @@ function StepDisplay() {
                 </footer>
             </section>
             <nav>
-                <button className="next Interactable"/>
-                <button className="last Interactable"/>
+                <button className="next Interactable" onClick={nextStep}/>
+                <button className="last Interactable" onClick={previousStep}/>
             </nav>
         </div>
     );
