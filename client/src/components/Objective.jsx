e7184524af9392ac90657ed4bedb0339dc104956
Lincoln Wentz <linkwentz@gmail.com>
2023-11-09 14:54:32 -0600

    Add content editing to objectives

--- client/src/components/Objective.jsx
+++ client/src/components/Objective.jsx
@@ -1,18 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { CompleteButton, DefeatButton, DeleteButton } from './StatusButtons';
+import EditableText from './EditableText';
 import API from '../scripts/API';
 
 function Objective(props) {
     
     const [content, setContent] = useState({
         statement: props.children,
-        completed: props.completed || null
+        completed: props.completed != undefined ? props.completed : null
     });
+
+    const updateObjectiveData = ({ statement, completed }) => {
+        API.update.objective(props.objectiveID, {statement, completed});
+    }
 
     const deleteObjective = async () => {
         await API.delete.objective(props.objectiveID);
         props.afterObjectiveDeletion(props.selected);
     };
+
+    useEffect(() => {
+        updateObjectiveData(content);
+    }, [content]);
 
     return (
         <div className={
@@ -21,7 +30,7 @@ function Objective(props) {
             ${content.completed == true ? 'Complete' : ''} 
             ${content.completed == false ? 'Defeat' : ''}`} 
         onClick={props.onClick}>
-            {content.statement}
+            <EditableText placeholder='Objective Statement' onContentChange={(newText) => {setContent({ ...content, statement: newText})}} selected={props.selected} onlyEditableIfSelected>{props.children}</EditableText>
             <CompleteButton content={content} setContent={setContent} selected={props.selected} onlyClickableIfSelected/>
             <DefeatButton content={content} setContent={setContent} selected={props.selected} onlyClickableIfSelected/>
             <DeleteButton delete={deleteObjective} selected={props.selected} onlyClickableIfSelected/>
